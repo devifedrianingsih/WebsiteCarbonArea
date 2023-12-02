baa61e6eecb9004d1c17ba76f7f8e704624628a3
Devi Fedrianingsih <91269321+devifedrianingsih@users.noreply.github.com>
2023-12-02 13:18:54 +0700

    Add files via upload

---
+++ Ahli Tanaman/ahli_tanaman.php
@@ -0,0 +1,234 @@
+<title>Ahli Tanaman</title>
+<div class=" nav-bar" >
+  <nav class="navbar navbar-expand-lg navbar-light bg-white ">
+      <header>
+          <a href="" class="navbar-brand m-0 p-0">
+              <h1 style="color: green;">CarbonArea</h1>
+          </a>
+      </header>
+
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+          <div class="navbar-collapse">
+              </div>
+              <a href="login.php" class="nav-item nav-link" style="color: green;"><b>Log Out</b></a> 
+          </div>
+      </div>
+  </nav>
+</div>
+
+<div class="container py-5 px-4">
+  <header class="text-left">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+  </header>
+
+  <div class="row rounded-lg overflow-hidden shadow">
+    <!-- Users box-->
+    <div class="col-5 px-0">
+      <div class="bg-white">
+
+        <div class="bg-gray px-4 py-2 bg-light">
+          <p class="h5 mb-0 py-1">Keluhan Petani</p>
+        </div>
+
+        <div class="messages-box">
+          <div class="list-group rounded-0">
+            <a class="list-group-item list-group-item-action active text-white rounded-0">
+              <div class="media"><img src="https://bootstrapious.com/i/snippets/sn-chat/avatar.svg" alt="user" width="50" class="rounded-circle">
+                <div class="media-body ml-4">
+                  <div class="d-flex align-items-center justify-content-between mb-1">
+                    <h6 class="mb-0">Budi Santoso</h6><small class="small font-weight-bold">27 Okt</small>
+                  </div>
+                  <p class="font-italic mb-0 text-small">Tanaman padi dan cabai saya mengalami serangan..</p>
+                </div>
+              </div>
+            </a>
+
+            <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
+              <div class="media"><img src="https://bootstrapious.com/i/snippets/sn-chat/avatar.svg" alt="user" width="50" class="rounded-circle">
+                <div class="media-body ml-4">
+                  <div class="d-flex align-items-center justify-content-between mb-1">
+                    <h6 class="mb-0">Joko Pranoto</h6><small class="small font-weight-bold">27 Okt</small>
+                  </div>
+                  <p class="font-italic text-muted mb-0 text-small">Saya memiliki lahan pertanian yang cukup be...</p>
+                </div>
+              </div>
+            </a>
+
+            <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
+              <div class="media"><img src="https://bootstrapious.com/i/snippets/sn-chat/avatar.svg" alt="user" width="50" class="rounded-circle">
+                <div class="media-body ml-4">
+                  <div class="d-flex align-items-center justify-content-between mb-1">
+                    <h6 class="mb-0">Mulyono Wibowo</h6><small class="small font-weight-bold">27 Okt</small>
+                  </div>
+                  <p class="font-italic text-muted mb-0 text-small">Saya membutuhkan bimbingan lebih lanjut me...</p>
+                </div>
+              </div>
+            </a>
+
+            <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
+              <div class="media"><img src="https://bootstrapious.com/i/snippets/sn-chat/avatar.svg" alt="user" width="50" class="rounded-circle">
+                <div class="media-body ml-4">
+                  <div class="d-flex align-items-center justify-content-between mb-1">
+                    <h6 class="mb-0">Agus Setiawan</h6><small class="small font-weight-bold">26 Okt</small>
+                  </div>
+                  <p class="font-italic text-muted mb-0 text-small">Saya sering bingung dalam memilih varietas ta...</p>
+                </div>
+              </div>
+            </a>
+
+            <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
+              <div class="media"><img src="https://bootstrapious.com/i/snippets/sn-chat/avatar.svg" alt="user" width="50" class="rounded-circle">
+                <div class="media-body ml-4">
+                  <div class="d-flex align-items-center justify-content-between mb-1">
+                    <h6 class="mb-0">Hadi Nugroho</h6><small class="small font-weight-bold">26 Okt</small>
+                  </div>
+                  <p class="font-italic text-muted mb-0 text-small">Saya berharap Anda dapat memberikan rekomendasi...</p>
+                </div>
+              </div>
+            </a>
+
+            <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
+              <div class="media"><img src="https://bootstrapious.com/i/snippets/sn-chat/avatar.svg" alt="user" width="50" class="rounded-circle">
+                <div class="media-body ml-4">
+                  <div class="d-flex align-items-center justify-content-between mb-1">
+                    <h6 class="mb-0">Dian Purnama</h6><small class="small font-weight-bold">25 Okt</small>
+                  </div>
+                  <p class="font-italic text-muted mb-0 text-small">Saya tidak yakin apakah komposisi tanah saya...</p>
+                </div>
+              </div>
+            </a>
+
+            <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
+              <div class="media"><img src="https://bootstrapious.com/i/snippets/sn-chat/avatar.svg" alt="user" width="50" class="rounded-circle">
+                <div class="media-body ml-4">
+                  <div class="d-flex align-items-center justify-content-between mb-1">
+                    <h6 class="mb-0">Rina Indah</h6><small class="small font-weight-bold">22 Okt</small>
+                  </div>
+                  <p class="font-italic text-muted mb-0 text-small">Saya sudah mencoba berbagai metode penge...</p>
+                </div>
+              </div>
+            </a>
+
+            <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
+              <div class="media"><img src="https://bootstrapious.com/i/snippets/sn-chat/avatar.svg" alt="user" width="50" class="rounded-circle">
+                <div class="media-body ml-4">
+                  <div class="d-flex align-items-center justify-content-between mb-1">
+                    <h6 class="mb-0">Jason Damono</h6><small class="small font-weight-bold">22 Okt</small>
+                  </div>
+                  <p class="font-italic text-muted mb-0 text-small">Apa teknik terbaik untuk irigasi, terutama dalam...</p>
+                </div>
+              </div>
+            </a>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Chat Box-->
+    <div class="col-7 px-0">
+      <div class="px-4 py-5 chat-box bg-white">
+        <!-- Sender Message-->
+        <div class="media w-50 mb-3"><img src="https://bootstrapious.com/i/snippets/sn-chat/avatar.svg" alt="user" width="50" class="rounded-circle">
+          <div class="media-body ml-3">
+            <div class="bg-light rounded py-2 px-3 mb-2">
+              <p class="text-small mb-0 text-muted">Tanaman padi dan cabai saya mengalami serangan hama yang cukup parah dalam beberapa minggu terakhir. Saya sudah mencoba berbagai metode pengendalian hama yang umum, namun tampaknya tidak memberikan hasil yang memuaskan. Saya butuh saran dari Anda mengenai strategi yang lebih efektif dalam mengatasi masalah ini.</p>
+            </div>
+            <p class="small text-muted">12:00 PM | 27 Okt</p>
+          </div>
+        </div>
+
+        <!-- Reciever Message-->
+        <div class="media w-50 ml-auto mb-3">
+          <div class="media-body">
+            <div class="bg-primary rounded py-2 px-3 mb-2">
+              <p class="text-small mb-0 text-white">Saya memahami kesulitan yang Anda alami dengan serangan hama pada tanaman padi dan cabai Anda. Masalah ini memang sering dihadapi oleh para petani. Berikut adalah beberapa saran untuk mengatasi masalah ini: Jika serangan hama sudah parah, penggunaan pestisida mungkin 
+diperlukan. Pastikan Anda menggunakan pestisida yang tepat untuk jenis hama yang Anda hadapi.</p>
+            </div>
+            <p class="small text-muted">12:02 PM | 27 Okt</p>
+          </div>
+        </div>
+
+        <!-- Sender Message-->
+        <div class="media w-50 mb-3"><img src="https://bootstrapious.com/i/snippets/sn-chat/avatar.svg" alt="user" width="50" class="rounded-circle">
+          <div class="media-body ml-3">
+            <div class="bg-light rounded py-2 px-3 mb-2">
+              <p class="text-small mb-0 text-muted">Bagaimana kalau penggunaan pestisida sudah dilakukan tetapi serangan hama masih belum juga mereda. Adakah cara lain?</p>
+            </div>
+            <p class="small text-muted">12:05 PM | 27 Okt</p>
+          </div>
+        </div>
+
+        <!-- Reciever Message-->
+        <div class="media w-50 ml-auto mb-3">
+          <div class="media-body">
+            <div class="bg-primary rounded py-2 px-3 mb-2">
+              <p class="text-small mb-0 text-white">Jika penggunaan pestisida tidak berhasil mengendalikan serangan hama, ada beberapa langkah yang bisa diambil. Berikut beberapa opsi yang dapat dipertimbangkan:
+<br/>
+1. Pemilihan Pestisida yang Tepat: Pastikan bahwa Anda telah menggunakan pestisida yang sesuai untuk jenis hama yang menyerang tanaman Anda. Konsultasikan dengan ahli pertanian atau spesialis hama dan penyakit untuk memastikan pemilihan pestisida yang benar.
+<br/>
+2. Rotasi Pestisida: Hama tertentu mungkin menjadi resisten terhadap pestisida tertentu. Cobalah rotasi atau penggantian jenis pestisida untuk menghindari resistensi hama.
+<br/>
+3. Pengendalian Biologis: Pertimbangkan penggunaan musuh alami hama, seperti predator atau parasitoid, untuk mengendalikan populasi hama. Ini bisa melibatkan pelepasan musuh alami ke lahan pertanian atau menciptakan habitat untuk mempromosikan populasi musuh alami.
+<br/>
+4. Teknik Pengendalian Fisik: Beberapa hama dapat dikendalikan dengan metode fisik seperti jaring serangga, perangkap, atau penggunaan cahaya ultraviolet (UV) malam.
+<br/>
+5. Budidaya Organik: Jika memungkinkan, pertimbangkan untuk beralih ke praktik pertanian organik. Pertanian organik fokus pada penggunaan pestisida alami dan pengelolaan ekosistem yang sehat.
+<br/>
+6. Pengendalian Sanitasi: Pastikan lahan pertanian Anda bersih dari sumber makanan atau tempat persembunyian hama. Ini termasuk menghilangkan tanaman yang terserang hama, menghilangkan gulma, dan menjaga kebersihan alat pertanian.
+<br/>
+7. Pengawasan Rutin: Lakukan pengawasan rutin pada lahan pertanian Anda untuk mendeteksi tanda-tanda serangan hama sejak dini. Semakin cepat tindakan diambil, semakin baik.
+<br/>
+8. Konsultasi dengan Ahli: Jika serangan hama terus berlanjut, penting untuk berkonsultasi dengan ahli pertanian atau spesialis hama dan penyakit. Mereka dapat memberikan saran yang lebih spesifik dan solusi yang sesuai dengan situasi Anda.
+<br/>
+Penting untuk diingat bahwa penggunaan pestisida harus selalu dilakukan sesuai dengan panduan label dan rekomendasi spesialis. Terlalu banyak penggunaan pestisida dapat berdampak negatif pada lingkungan dan kesehatan manusia. Dengan demikian, menggabungkan berbagai metode pengendalian hama yang lebih berkelanjutan bisa menjadi pilihan yang lebih baik dalam jangka panjang.</p>
+            </div>
+            <p class="small text-muted">12:08 PM | 27 Okt</p>
+          </div>
+        </div>
+
+        <!-- Sender Message-->
+        <div class="media w-50 mb-3"><img src="https://bootstrapious.com/i/snippets/sn-chat/avatar.svg" alt="user" width="50" class="rounded-circle">
+          <div class="media-body ml-3">
+            <div class="bg-light rounded py-2 px-3 mb-2">
+              <p class="text-small mb-0 text-muted">Baik. Terima Kasih atas sarannya!</p>
+            </div>
+            <p class="small text-muted">12:10 PM | 27 Okt</p>
+          </div>
+        </div>
+
+        <!-- Reciever Message-->
+        <div class="media w-50 ml-auto mb-3">
+          <div class="media-body">
+            <div class="bg-primary rounded py-2 px-3 mb-2">
+              <p class="text-small mb-0 text-white">Terima kasih kembali. Jika ada kendala atau ada yang ingin ditanyakan jangan sungkan untuk berkonsultasi dengan Kami. <br/> Salam, CarbonArea.</p>
+            </div>
+            <p class="small text-muted">12:12 PM | 27 Okt</p>
+          </div>
+        </div>
+      </div>
+
+      <!-- Typing area -->
+      <form action="#" class="bg-light">
+        <div class="input-group">
+          <input type="text" placeholder="Type a message" aria-describedby="button-addon2" class="form-control rounded-0 border-0 py-4 bg-light">
+          <div class="input-group-append">
+            <button id="button-addon2" type="submit" class="btn btn-link"> <i class="fa fa-paper-plane"></i></button>
+          </div>
+        </div>
+      </form>
+
+    </div>
+  </div>
+</div>
+
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
